9e1671288871f966d356631d762d4b4b82e4206b
thakeryash <yashthaker8182@gmail.com>
2023-09-29 13:35:48 +0530

    html code
    html code

---
+++ js/new.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+        infinite: false,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        arrows: true,
+        
+      });
+});
